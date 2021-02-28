e3969a7a5ad87b2e3cce128a5723ea02d77d2dab
fffk10 <warafffk10@gmail.com>
2021-02-28 23:37:39 +0900

    宛先アドレスと本文入力欄を実装

--- app/src/main/java/com/example/mail/MainActivity.kt
+++ app/src/main/java/com/example/mail/MainActivity.kt
@@ -7,23 +7,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText
 import android.widget.Toast;
 import java.nio.channels.AsynchronousByteChannel
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var toAddress: EditText
+    private lateinit var content: EditText
+    private lateinit var sendBtn: Button
+
+    private lateinit var strToAddress: String
+    private lateinit var strContent: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val task = SendMail(
-                fromAddress = "warafffk10@gmail.com",
-                password = "emxfwiryektqdkea",
-                toAddress = "02hrmr02@gmail.com",
-                subject = "試し送り",
-                text = "わらさんだよ"
-        )
-        task.execute()
+        toAddress = findViewById(R.id.toAddress)
+        content = findViewById(R.id.content)
+
+        sendBtn = findViewById(R.id.button)
+
+        // ボタン送信でメールを送信する
+        sendBtn.setOnClickListener() {
+            // ボタン送信で入力された宛先と本文を入力
+            strToAddress = toAddress.text.toString()
+            strContent = content.text.toString()
+
+            // 送信情報
+            val task = SendMail(
+                    fromAddress = "warafffk10@gmail.com",
+                    password = "emxfwiryektqdkea",
+                    toAddress = strToAddress,
+                    subject = "試し送り",
+                    text = strContent
+            )
+            task.execute()
+        }
     }
 }
